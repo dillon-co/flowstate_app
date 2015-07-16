16607a4fe4f7247945ac884e258eced8c31b13c7
dilloncortez <dcortez@peer60.com>
2015-07-16 11:28:21 -0600

    fixed devise error?

--- config/application.rb
+++ config/application.rb
@@ -19,12 +19,13 @@ module Flowstate
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
     # For Foundation 5
     config.assets.precompile += %w( vendor/modernizr )
 
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.initialize_on_precompile = false
+
   end
 end
